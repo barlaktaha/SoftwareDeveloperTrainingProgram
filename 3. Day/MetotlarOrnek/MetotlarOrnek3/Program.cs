4d2243804f0d959c04decb7d81a5564734efd38b
barlaktaha <irtibat.tahabarlak@gmail.com>
2021-01-19 21:24:21 +0300

    Create Program.cs

---
+++ 3. Day/MetotlarOrnek/MetotlarOrnek3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MetotlarOrnek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Add5(1,2,3,4,5,6,7,8,9));
+            Console.ReadLine();
+        }
+
+        static int Add5(params int[] numbers)
+        {
+
+            return numbers.Sum();
+            
+
+        }
+    }
+}
