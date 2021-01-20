dadedc9c208d172f83ef8b85b2641c844aef792b
barlaktaha <irtibat.tahabarlak@gmail.com>
2021-01-20 20:29:03 +0300

    Create Program.cs

---
+++ 3. Day/Odev3/ClassMetotDemo/ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri = new Musteri();
+            musteri.MusteriAdi = "Taha";
+            musteri.MusteriId = 123456;
+            musteri.HesapTuru = "vadeli";
+
+            Console.WriteLine(musteri.MusteriAdi + "\n" + musteri.MusteriId + "\n" + musteri.HesapTuru);
+
+            MusteriManager musterimanager = new MusteriManager();
+
+            musterimanager.Silme(musteri);
+
+        }
+    }
+    class Musteri
+    {
+        public string MusteriAdi { get; set; }
+        public int MusteriId { get; set; }
+        public string HesapTuru { get; set; }
+    }
+
+    class MusteriManager
+    {
+        public void Silme(Musteri musteri)
+        {
+            Console.WriteLine("Hesabınız Silindi: " + musteri.MusteriAdi + " Bey ");
+        }
+    }
+}
